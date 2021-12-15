d1b18e0cf989f639466621efadda775c7ed8b358
fresanrez <80113369+fresanrez@users.noreply.github.com>
2021-12-15 16:46:07 -0500

    rechimba de localizacion

--- app/src/main/java/com/fivetic/recycler/Registro.java
+++ app/src/main/java/com/fivetic/recycler/Registro.java
@@ -2,13 +2,22 @@ package com.fivetic.recycler;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.regex.* ;
 import com.fivetic.recycler.Constantes;
@@ -34,6 +43,9 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
     private EditText editText_direccion ;
     private EditText editText_ciudad ;
     private Button btn_acept ;
+    /*private LocationManager ubicacion;
+    private TextView TextViewlatitud;
+    private TextView TextViewlongitud;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +62,8 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
         editText_direccion = findViewById(R.id.editText_direccion) ;
         btn_acept = findViewById(R.id.btn_acept) ;
 
-
+        /*localizacion();
+        registrarLocalizacion();*/
         btn_acept.setOnClickListener(this);
     }
 
@@ -79,6 +92,9 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
         Matcher mat_clave_alfa = pat_alfa.matcher(clave);
         boolean match_found_clave_alfa = mat_clave_alfa.find();
 
+        // implementar geolocalizacion
+        // funciones de localizacion
+
         if(clave.length()>=8){
 
               if (match_found_clave_esp || match_found_clave_num || match_found_clave_alfa){
@@ -93,6 +109,67 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
         }
 }
 
+
+    /*private void localizacion(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION
+            },1000);
+        }
+        TextViewlongitud= findViewById(R.id.longitud);
+        TextViewlatitud= findViewById(R.id.latitud);
+        ubicacion=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location loc=ubicacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (ubicacion!=null){
+            Log.d("latitud",String.valueOf(loc.getLatitude()));
+            Log.d("longitud",String.valueOf(loc.getLongitude()));
+        }
+    }
+
+    private void registrarLocalizacion() {
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        ubicacion.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, new milocalizacionListener());
+    }
+
+    private class milocalizacionListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            double lat =location.getLatitude();
+            double lon =location.getLongitude();
+            TextViewlongitud.setText(String.valueOf(lon));
+            TextViewlatitud.setText(String.valueOf(lat));
+        }
+
+
+        @Override
+        public void onLocationChanged(@NonNull List<Location> locations) {
+
+        }
+
+        @Override
+        public void onFlushComplete(int requestCode) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
+        }
+    }*/
 
 
     public void registarUsuario( String tipo,  String nombre, String correo, String clave, String direccion, String ciudad){
